17413d2b4ed5d301095c93c9620d5290ff6be0dc
Sergey <Sergey40248@mail.ru>
2022-11-28 00:57:10 +0300

    task2_modified

--- task2.py
+++ task2.py
@@ -4,9 +4,6 @@
 #Для n = 15: Ответ: 3
 #Для n = 35: Ответ: 5
 
-
-
-
 devident = int(input('enter the devident:  '))
 
 if devident == 1:
@@ -14,7 +11,7 @@ if devident == 1:
         
 i = 2
 while devident % i != 0:
-    i+=1
+    i += 1
 
 print(f'smallest natural divisor = {i}')    
         

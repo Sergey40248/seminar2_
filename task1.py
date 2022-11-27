d1637ff5fd17d1b7c0b1d478539d27fa69c81419
Sergey <Sergey40248@mail.ru>
2022-11-28 00:56:07 +0300

    Task1_modified

--- task1.py
+++ task1.py
@@ -3,13 +3,14 @@
 #Пример:
 #пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
-n = int(input('enter number\n'))
+n = int(input('enter the number\n'))
 count = 0
 f = 1
-a = []
+a = []       #чтобы вывод выглядел как в задании 
 while count < n:
     count += 1
     f *= count
-    a.append(f)#чтобы вывод выглядел как в задании 
+   
+    a.append(f)  #чтобы вывод выглядел как в задании 
 print(a)
-    
+
